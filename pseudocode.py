0130c5d05769e566a54390d4fe6fb81770c72bc6
QuintusLamar <qlamar3@gatech.edu>
2023-12-01 19:08:23 -0500

    Changed pseudocode to find threshold rather than exact matches

--- pseudocode.py
+++ pseudocode.py
@@ -43,22 +43,18 @@ def calculate_scores(scores_arr, sentences_a, sentences_b, model):
   return scores_arr
 
 
-def remove_questions(scores_arr, question_a, question_b):
+def remove_questions(scores_arr, question_a, question_b, model):
   to_remove = []
 
   for score in scores_arr:
-    threshold = 0.7
-    # If we want to implement threshold, we can have the following:
-    # if cosine(score[0], question_a) < threshold or cosine(score[1], question_b) < threshold
+    threshold = 0.75
 
-    # I think that we should probably compare score[0] to question_b as well as score[1] to question_a
-    # The reason I am not so far is because I looked for exact matches, but since we probably want threshold then
-    # it would be necessary to compare the 4 possible pairs between score[0], score[1] and question_a, question_b
-
-    if score[0] == question_a or score[1] == question_b:
+    if cosine(model.encode(score[0]), model.encode(question_a)) >= threshold and \
+        cosine(model.encode(score[0]), model.encode(question_b)) >- threshold and \
+        cosine(model.encode(score[1]), model.encode(question_a)) >= threshold and \
+        cosine(model.encode(score[1]), model.encode(question_b)) >- threshold:
       to_remove.append(score)
 
-  # Removes all of the removable scores
   for score in to_remove:
     scores_arr.remove(score)
   return scores_arr
@@ -74,9 +70,9 @@ def chooseBestNQuestions(first, second, n):
     chosen_score = new_scores[0]
     chosen.append(chosen_score[0])
     new_scores.remove(chosen_score)
-    new_scores = remove_questions(new_scores, chosen_score[0], chosen_score[1])
+    new_scores = remove_questions(new_scores, chosen_score[0], chosen_score[1], model)
   
   return chosen
 
-chosen = chooseBestNQuestions(first_model, second_model, 3)
-print("Chosen: ", chosen)
+# chosen = chooseBestNQuestions(first_model, second_model, 3)
+# print("Chosen: ", chosen)
